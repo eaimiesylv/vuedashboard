b413dfc7895fe4f9794eb0d3893a0bcbe9cab445
Emmanuel Okum <emmanuel.okum@idea2solutions.com>
2023-12-30 04:52:11 +0100

    Initial set up

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <AdminDashboard />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AdminDashboard from './components/AdminDashboard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AdminDashboard
   }
 }
 </script>
---
+++ src/components/AdminDashboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+      <Aside>
+          <a href="#">Home page</a>
+      </Aside>
+      <Main>
+          This is the body of the home page
+      </Main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminDashboard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
