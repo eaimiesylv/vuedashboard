1fd5b6d6d0258ce3ef0487430dd1b234f6d817c5
Emmanuel Okum <emmanuel.okum@idea2solutions.com>
2023-12-31 20:14:48 +0100

    converted css file to less file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/style.less'; 
 const app = createApp(App);
 //app.component('base-layout', BaseLayout);
 app.use(router);
